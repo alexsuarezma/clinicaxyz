23f07dcd6e7d0311c4125561f8f7113e44e86034
Randy <randybolanos250@gmail.com>
2020-06-23 13:32:18 -0500

    modulo pacientes form sin diseño funcionando test1

--- app/modulos/pacientes/conexion.php
+++ app/modulos/pacientes/conexion.php
@@ -1,32 +1,16 @@
-<?
-include 'baemini.php';
-$cedula = $_POST["cedula"];
-$nombre = $_POST["fnom"];
-$apellidop = $_POST["fapell11"];
-$apellidom = $_POST["fapell2"];
-$fech_nac = $_POST["fnac"];
-$ocupacion = $_POST["ocup"];
-$sexo = $_POST["gender"];
-$ciudad = $_POST["citi"];
-$telefono = $_POST["ftel"];
-$celular = $_POST["fcel"];
-$email = $_POST["fmail"];
-$direc = $_POST["fdir"];
-$afil = $_POST["afil"];
-$disca = $_POST["discap"];
-$conad = $_POST["conad"];
-$discapa = $_POST["discapa"];
-$grado = $_POST["grado"];
+<?php
+$conectar = mysqli_connect ('us-cdbr-east-05.cleardb.net','b7550b2dcd9c38','a16e5057','heroku_fe7e002859673b2');
 
-$insertar = "INSERT INTO pacientes (idpacientes,ape_paterno,ape_mat,nombres,ocupación,sexo,f_nacimiento,provincia,ciudad,zona,dirección,tlno_particular,tlno_personal,correo) VALUES ('$cedula','$apellidop','$apellidom','$nombre','$ocupacion','$sexo','$fech_nac','','$ciudad','','$direc','$telefono','$celular','$email')";
-$resultado= mysqli_query($conex,$insertar)
-if (!$resultado){
-    echo 'error al registrarse';
-}
-else{
-    echo 'usuario registrado exitosamente';
+if(!$conectar){
+    echo"no se pudo conectar a la base de datos";
+}else{
+    
+    echo"conexion exitosa";
 }
 
-mysqli_close($conexion);
-/**$s = $_POST[""]**/
-    ?>
+
+
+
+
+
+?>
